b8d7c5c4dae413a511c67abb78304117f6497328
Chathuranga Dassanayake <chathura2ihl@gmail.com>
2023-09-26 21:39:58 +0530

    w

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/chathurangakcd/file-reader-writer
 go 1.21.1
 
 require (
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- main.go
+++ main.go
@@ -1,22 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/util/uuid"
 )
 
+var serverId string
+
 func main() {
+	serverId = generateUUID()
 	http.HandleFunc("/writeFile", writeFile)
 	http.HandleFunc("/readFile", readFile)
+	http.HandleFunc("/listFiles", listFiles)
+	http.HandleFunc("/deleteFile", deleteFile)
 
 	http.ListenAndServe(":8080", nil)
 }
 
+func generateUUID() string {
+	id, _ := uuid.NewRandom()
+	return id.String()
+}
+func writeJSON(w http.ResponseWriter, msg string, requestId string, data interface{}) {
+	res := map[string]interface{}{
+		"message":   msg,
+		"serverId":  serverId,
+		"requestId": requestId,
+		"data":      data,
+	}
+	responseData, err := json.Marshal(res)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to create JSON response: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(responseData))
+}
+
 func writeFile(w http.ResponseWriter, r *http.Request) {
+	requestId := generateUUID()
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -27,18 +55,20 @@ func writeFile(w http.ResponseWriter, r *http.Request) {
 	logrus.WithFields(logrus.Fields{
 		"filePath":    filePath,
 		"fileContent": fileContent,
+		"requestId":   requestId,
+		"serverId":    serverId,
 	}).Info("Writing file")
 
-	err := ioutil.WriteFile(filePath, []byte(fileContent), 0644)
+	err := os.WriteFile(filePath, []byte(fileContent), 0644)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Unable to write to file: %s", err.Error()), http.StatusInternalServerError)
 		return
 	}
-
-	w.Write([]byte("File written successfully"))
+	writeJSON(w, "File written successfully", requestId, nil)
 }
 
 func readFile(w http.ResponseWriter, r *http.Request) {
+	requestId := generateUUID()
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -46,10 +76,12 @@ func readFile(w http.ResponseWriter, r *http.Request) {
 
 	filePath := r.FormValue("filePath")
 	logrus.WithFields(logrus.Fields{
-		"filePath": filePath,
+		"filePath":  filePath,
+		"requestId": requestId,
+		"serverId":  serverId,
 	}).Info("Reading file")
 
-	data, err := ioutil.ReadFile(filePath)
+	data, err := os.ReadFile(filePath)
 	if err != nil {
 		if os.IsNotExist(err) {
 			http.Error(w, "File not found", http.StatusNotFound)
@@ -60,4 +92,66 @@ func readFile(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write(data)
+}
+
+func listFiles(w http.ResponseWriter, r *http.Request) {
+	requestId := generateUUID()
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	dirPath := r.FormValue("dirPath")
+	logrus.WithFields(logrus.Fields{
+		"dirPath":   dirPath,
+		"requestId": requestId,
+		"serverId":  serverId,
+	}).Info("Listing files")
+
+	files, err := os.ReadDir(dirPath)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Unable to read directory: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	var fileNames []string
+	for _, file := range files {
+		fileNames = append(fileNames, file.Name())
+	}
+
+	// Return the file names as a JSON response
+	responseData, err := json.Marshal(fileNames)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to create JSON response: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	writeJSON(w, "Files listed successfully", requestId, responseData)
+}
+
+// New function to handle file deletion
+func deleteFile(w http.ResponseWriter, r *http.Request) {
+	requestId := generateUUID()
+	if r.Method != http.MethodDelete {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	filePath := r.FormValue("filePath")
+	logrus.WithFields(logrus.Fields{
+		"filePath":  filePath,
+		"requestId": requestId,
+		"serverId":  serverId,
+	}).Info("Deleting file")
+
+	err := os.Remove(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.Error(w, "File not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, fmt.Sprintf("Unable to delete file: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+	writeJSON(w, "File deleted successfully", requestId, nil)
 }
